abae012550e4586366a3d1e6bf7fc7a81df3016d
Mahdi Hamdani <mahdi@Mahdis-MBP.home>
2019-11-28 07:56:43 +0100

    Added seq evaluator

--- nex_cv/data_providers.py
+++ nex_cv/data_providers.py
@@ -1,5 +1,6 @@
 import numpy as np
 from collections import Counter
+from datetime import datetime
 
 
 class TestingDatasetProvider:  # pylint: disable=too-few-public-methods
@@ -47,7 +48,7 @@ class NegativeExamplesDatasetProvider(TestingDatasetProvider):  # pylint: disabl
         self.min_category_size = min_category_size
         self.other_min_prop = other_min_prop
 
-        self.splitter = self._split_labels_by_size if min_category_size > 0 else\
+        self.splitter = self._split_labels_by_size if min_category_size > 0 else \
             self._split_labels_by_prop
 
         self.test_fraction = test_fraction
@@ -142,3 +143,168 @@ class NegativeExamplesDatasetProvider(TestingDatasetProvider):  # pylint: disabl
         return super().__str__() + "(%s;%s)" % (self.min_category_size,
                                                 self.other_min_prop)
 
+
+class TimeSeqDatasetProvider(TestingDatasetProvider):
+    """
+    The training data is grown gradually using the creation date for sorting the data
+    "created" is expected to be in the "intent" annotation dict
+    """
+    DATE_KEY = "created"
+    INTENT_KEY = "intent_id"
+
+    def __init__(self, min_examples_ratio=0.3, max_added_examples=None, n_steps=5, date_key=DATE_KEY,
+                 test_on_all=False, test_ratio=0.33):
+        """
+
+        :param min_examples_ratio: (float) ratio of minimum number of training examples
+        :param max_added_examples: (int) maximum number of added examples
+        :param n_steps: (int) number of steps in the learning curve
+        :param date_key: (str) key used for sorting samples
+        :param test_on_all: (bool) if False test set is sampled only from the time covered by training set
+                                  if True test set is sampled from all data
+        :param test_ratio: (float) test data ratio
+        """
+        self.min_examples_ratio = min(min_examples_ratio, 0.8)
+        self.max_added_examples = max_added_examples
+        self.n_steps = n_steps
+        if self.n_steps <= 0:
+            self.n_steps = 1
+
+        self.test_on_all = test_on_all
+        self.test_step = int(1. / test_ratio)
+        self.date_key = date_key
+
+    @staticmethod
+    def resolve_date(a, date_key):
+        """
+
+        :param a: list(dict) with annotations
+        :param date_key: str key used to extract date
+        :return: timestamp extracted from date
+        """
+
+        try:
+            d = a.get(date_key)
+            return datetime.timestamp(d)
+        except:
+            pass
+        return None
+
+    @staticmethod
+    def resolve_intent(a, intent_key):
+        """
+
+        :param a:
+        :param intent_key:
+        :return:
+        """
+        try:
+            intent = a.get(intent_key)
+            return intent
+        except:
+            pass
+        return None
+
+    @staticmethod
+    def sort_dataset_by_date(X, y, date_key, return_timestamps=False):
+        """
+
+        :param X: list(str): training sentences
+        :param y: list(dict): with intent_id and created (and updated) dates
+        :param date_key: key used for the date used in sorting the data
+        :param return_timestamps: if the timestamps are returned (e.g. used by the data provider)
+        :return: sorted dataset: X, y[, time_stamps]
+        """
+
+        time_stamps = [TimeSeqDatasetProvider.resolve_date(a, date_key) for a in y]
+        kept_indices = [i for i in range(len(time_stamps)) if time_stamps[i] is not None]
+        if len(kept_indices) != len(time_stamps):
+            X = np.take(X, kept_indices).tolist()
+            y = np.take(y, kept_indices).tolist()
+            time_stamps = np.take(time_stamps, kept_indices).tolist()
+
+        # Return empty datasets
+        if len(y) != len(time_stamps) != len(X) or len(y) == 0:
+            if return_timestamps:
+                return [], [], []
+            else:
+                return [], []
+
+        y = [TimeSeqDatasetProvider.resolve_intent(y_i, TimeSeqDatasetProvider.INTENT_KEY) for y_i in y]
+
+        sorted_indices = np.argsort(time_stamps)
+
+        X = np.array(X)[sorted_indices].tolist()
+        y = np.array(y)[sorted_indices].tolist()
+
+        if return_timestamps:
+            time_stamps = np.array(time_stamps)[sorted_indices].tolist()
+            return X, y, time_stamps
+
+        return X, y
+
+    def get_datasets(self, X, y):  # pylint: disable=invalid-name
+        """
+
+        :param X: list: training text
+        :param y: list: intents
+        :return: list: training and test datasets
+        """
+
+        X, y, time_stamps = self.sort_dataset_by_date(X, y, self.date_key, return_timestamps=True)
+
+        datasets = []
+        full_train_set = {"X": [], "y": [], "t": []}
+        full_test_set = {"X": [], "y": [], "t": []}
+
+        for sample_idx in range(len(X)):
+            if not sample_idx % self.test_step:
+                full_test_set["X"].append(X[sample_idx])
+                full_test_set["y"].append(y[sample_idx])
+                full_test_set["t"].append(time_stamps[sample_idx])
+            else:
+                full_train_set["X"].append(X[sample_idx])
+                full_train_set["y"].append(y[sample_idx])
+                full_train_set["t"].append(time_stamps[sample_idx])
+
+        n_training_examples = len(full_train_set["X"])
+        n_test_examples = len(full_test_set["X"])
+
+        min_examples = int(self.min_examples_ratio * n_training_examples)
+
+        test_cursor = 0
+        train_cursor = min_examples
+
+        # step_size: how many examples we add / evaluation
+        if self.max_added_examples and min_examples + self.max_added_examples < n_training_examples:
+            step_size = self.max_added_examples // self.n_steps
+        else:
+            step_size = (n_training_examples - min_examples) // self.n_steps
+        if step_size == 0:
+            # in case n_steps > number of training examples
+            step_size = 1
+
+        # Generate datasets
+        while train_cursor <= n_training_examples:  # and prev_train_cursor < n_training_examples:
+            sub_train_x = full_train_set["X"][:train_cursor]
+            sub_train_y = full_train_set["y"][:train_cursor]
+
+            if self.test_on_all:
+                datasets.append((sub_train_x, sub_train_y, full_test_set["X"], full_test_set["y"]))
+            else:
+                last_time_stamp = full_train_set["t"][train_cursor - 1]
+                while test_cursor < n_test_examples and full_test_set["t"][test_cursor] < last_time_stamp:
+                    test_cursor += 1
+                datasets.append((sub_train_x, sub_train_y,
+                                 full_test_set["X"][:test_cursor], full_test_set["y"][:test_cursor]))
+
+            if train_cursor >= n_training_examples or (self.max_added_examples is not None and
+                                                       train_cursor >= min_examples + self.max_added_examples):
+                break
+
+            train_cursor = min(n_training_examples, train_cursor + step_size)
+        return datasets
+
+    def __str__(self):
+        return super().__str__() + "(%s;%s,%s,%s)" % (self.min_examples_ratio, self.n_steps,
+                                                      self.test_on_all, self.test_step)
